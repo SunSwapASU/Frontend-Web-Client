4d499c1aa00dcccb552ec2963c6270cd461be0a2
Brian Longmore <branodev@gmail.com>
2024-09-28 16:00:33 -0700

    logout example

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const emailField = document.getElementById('email');
 const registerButton = document.getElementById('registerButton');
 const loginButton = document.getElementById('loginButton');
 const restrictedButton = document.getElementById('restrictedButton');
+const logoutButton = document.getElementById('logoutButton');
 
 registerButton.addEventListener('click', registerUser);
 loginButton.addEventListener('click', loginUser);
 restrictedButton.addEventListener('click', restricted);
+logoutButton.addEventListener('click', logout);
 
 
 async function loginUser(e) {
@@ -59,6 +61,23 @@ async function restricted(e) {
 
     let res = await fetch('http://127.0.0.1:3000/restricted', {
         method: 'GET',
+        mode: "cors",
+        credentials: "include",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    let msg = await res.json();
+
+    console.log(msg);
+}
+
+async function logout(e) {
+    e.preventDefault();
+
+    let res = await fetch('http://127.0.0.1:3000/logout', {
+        method: 'POST',
         mode: "cors",
         credentials: "include",
         headers: {
