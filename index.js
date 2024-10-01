90a3d15033c37b9b17f3b74326701a70bf836cf8
Brian Longmore <branodev@gmail.com>
2024-10-01 04:46:59 -0700

    aight

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const loginButton = document.getElementById('loginButton');
 const restrictedButton = document.getElementById('restrictedButton');
 const logoutButton = document.getElementById('logoutButton');
 
+const responseMessage = document.getElementById('response_message');
+
+
 registerButton.addEventListener('click', registerUser);
 loginButton.addEventListener('click', loginUser);
 restrictedButton.addEventListener('click', restricted);
@@ -16,32 +19,37 @@ logoutButton.addEventListener('click', logout);
 async function loginUser(e) {
     e.preventDefault();
 
-    username = usernameField.value;
-    password = passwordField.value;
-    email = emailField.value;
+    responseMessage.innerText = "Loading...";
 
-    let res = await fetch('http://127.0.0.1:3000/login', {
+    email = emailField.value;
+    password = passwordField.value;
+
+    let res = await fetch('http://127.0.0.1:3000/auth/login', {
         method: 'POST',
         mode: "cors",
         credentials: "include",
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ username, email, password }),
+        body: JSON.stringify({ email, password }),
     });
 
-    let msg = await res.json();
+    let { message } = await res.json();
 
-    console.log(msg);
+    responseMessage.innerText = message;
+
+    console.log(message);
 }
 async function registerUser(e) {
     e.preventDefault();
 
+    responseMessage.innerText = "Loading...";
+
     username = usernameField.value;
     password = passwordField.value;
     email = emailField.value;
 
-    let res = await fetch('http://127.0.0.1:3000/register', {
+    let res = await fetch('http://127.0.0.1:3000/auth/register', {
         method: 'POST',
         mode: "cors",
         credentials: "include",
@@ -51,15 +59,19 @@ async function registerUser(e) {
         body: JSON.stringify({ username, email, password }),
     });
 
-    let msg = await res.json();
+    let { message } = await res.json();
 
-    console.log(msg);
+    responseMessage.innerText = message;
+
+    console.log(message);
 }
 
 async function restricted(e) {
     e.preventDefault();
 
-    let res = await fetch('http://127.0.0.1:3000/restricted', {
+    responseMessage.innerText = "Loading...";
+
+    let res = await fetch('http://127.0.0.1:3000/private/user/getAll', {
         method: 'GET',
         mode: "cors",
         credentials: "include",
@@ -70,13 +82,17 @@ async function restricted(e) {
 
     let msg = await res.json();
 
+    responseMessage.innerText = JSON.stringify(msg);
+
     console.log(msg);
 }
 
 async function logout(e) {
     e.preventDefault();
 
-    let res = await fetch('http://127.0.0.1:3000/logout', {
+    responseMessage.innerText = "Loading...";
+
+    let res = await fetch('http://127.0.0.1:3000/auth/logout', {
         method: 'POST',
         mode: "cors",
         credentials: "include",
@@ -85,7 +101,9 @@ async function logout(e) {
         },
     });
 
-    let msg = await res.json();
+    let { message } = await res.json();
 
-    console.log(msg);
+    responseMessage.innerText = message;
+
+    console.log(message);
 }
